0b77aeb7e48310506b42b1a9c6316d6dbc12eabe
wisardsteri <visaso@metropolia.fi>
2018-04-20 12:41:41 +0300

    MusicPlayer

---
+++ RoboProject/src/MusicPlayer.java
@@ -0,0 +1,15 @@
+import lejos.hardware.Sound;
+
+public class MusicPlayer extends Thread {
+	
+	public MusicPlayer(String song) {
+		
+
+	}
+	
+	public void run() {
+		
+		//Sound.playTone(aFrequency, aDuration, aVolume);
+	}
+	
+}
