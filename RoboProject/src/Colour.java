5320d2c9ca783e758e3b0b7c2f8c9e6e702b38ee
Jukkakot <32321813+Jukkakot@users.noreply.github.com>
2018-04-20 13:04:10 +0300

    Muutettu color luokka

--- RoboProject/src/Colour.java
+++ RoboProject/src/Colour.java
@@ -8,16 +8,17 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.Color;
+import lejos.robotics.RegulatedMotor;
 
 public class Colour {
 	private EV3ColorSensor cs; 
 	private int colour;
-	private Drive clawMotor = new Drive(new EV3MediumRegulatedMotor(MotorPort.A));
-	private Drive driveMotor = new Drive(new EV3LargeRegulatedMotor(MotorPort.B), new EV3MediumRegulatedMotor(MotorPort.C));
+	//private Drive clawMotor = new Drive(new EV3MediumRegulatedMotor(MotorPort.A));
+	//private Drive driveMotor = new Drive(new EV3LargeRegulatedMotor(MotorPort.B), new EV3MediumRegulatedMotor(MotorPort.C));
 	public Colour (EV3ColorSensor cs) {
 		this.cs = cs;
 	}
-	public void setColour (int colour) {
+	public void setColour (int colour, RegulatedMotor mA) {
 		//VÄRIN MÄÄRITYS
 		this.colour = colour;
 			LCD.drawInt(cs.getColorID(), 0, 5);
@@ -34,20 +35,20 @@ public class Colour {
 				Sound.playTone(500, 500);
 			}
 			if(colour == 4) {
-				clawMotor.rotateClaw(-100);
+				mA.rotate(-100);
 			}
 	}
 	public int getColour () {
 		return colour;
 	}
 	//VÄRIN TUNNISTUS
-	public void findColour () {
+	public void findColour (RegulatedMotor mB, RegulatedMotor mA) { //SUURI, KOURA
 		switch(cs.getColorID()) {
 		case Color.BLUE:
 			if (colour == 2) {
 				colour = 0;
-				clawMotor.rotateClaw(-100); //sulje koura & pysäytä moottori
-				driveMotor.stop();
+				mA.rotate(-100); //sulje koura & pysäytä moottori
+				mB.stop();
 				LCD.clear(4);
 				LCD.drawString("BLUE", 0, 4);
 				Sound.playTone(500, 100);
@@ -59,8 +60,8 @@ public class Colour {
 		case Color.GREEN:
 			if (colour == 1) {
 				colour = 0;
-				clawMotor.rotateClaw(-100); //sulje koura & pysäytä moottori
-				driveMotor.stop();
+				mA.rotate(-100); //sulje koura & pysäytä moottori
+				mB.stop();
 				LCD.clear(4);
 				LCD.drawString("GREEN", 0, 4);
 				Sound.playTone(500, 100);
@@ -71,8 +72,8 @@ public class Colour {
 		case Color.RED:
 			if (colour == 3) {
 				colour = 0;
-				clawMotor.rotateClaw(-100); //sulje koura & pysäytä moottori
-				driveMotor.stop();
+				mA.rotate(-100); //sulje koura & pysäytä moottori
+				mB.stop();
 				LCD.clear(4);
 				LCD.drawString("Red", 0, 4);
 				Sound.playTone(500, 100);
--- RoboProject/src/Main.java
+++ RoboProject/src/Main.java
@@ -12,19 +12,19 @@ import lejos.utility.Delay;
 public class Main {
 	public static void main(String[] args) {
 		int i = 0;
-		//EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
-		//EV3IRSensor irSensor = new EV3IRSensor(SensorPort.S2);
-		//RegulatedMotor m1 = new EV3MediumRegulatedMotor(MotorPort.A); //KOURA (MEDIUM MOTOR)
-		//RegulatedMotor m2 = new EV3MediumRegulatedMotor(MotorPort.C); //OHJAUS (MEDIUM MOTOR)
-		//RegulatedMotor m3 = new EV3LargeRegulatedMotor(MotorPort.B); //AJO (LARGE MOTOR)
+		EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
+		EV3IRSensor irSensor = new EV3IRSensor(SensorPort.S2);
+		EV3MediumRegulatedMotor m1 = new EV3MediumRegulatedMotor(MotorPort.A); //KOURA (MEDIUM MOTOR)
+		EV3MediumRegulatedMotor m2 = new EV3MediumRegulatedMotor(MotorPort.C); //OHJAUS (MEDIUM MOTOR)
+		EV3LargeRegulatedMotor m3 = new EV3LargeRegulatedMotor(MotorPort.B); //AJO (LARGE MOTOR)
 		
 		//OHJAUS JA KOURA LUONTI
 		LCD.drawString("Ohjelma päällä", 0, 1);
-		Drive mBmCIrSensor = new Drive(new EV3LargeRegulatedMotor(MotorPort.B), new EV3MediumRegulatedMotor(MotorPort.C));
-		Drive mA = new Drive(new EV3MediumRegulatedMotor(MotorPort.A));
+		Drive mBmCIrSensor = new Drive(m3, m2);
+		Drive mA = new Drive(m1);
 		//Värisensorin käyttö
-		Colour colourSensor = new Colour(new EV3ColorSensor(SensorPort.S3)); 
-		IrsChecker irsChecker = new IrsChecker(new EV3IRSensor(SensorPort.S2));
+		Colour colourSensor = new Colour(cs); 
+		IrsChecker irsChecker = new IrsChecker(irSensor);
 		
 		while(true) {
 			//MODEN VAIHTO
@@ -37,10 +37,10 @@ public class Main {
 			mBmCIrSensor.driveWithController(irsChecker.getRemComValue(0));
 			//VÄRIN MÄÄRITYS
 			if(irsChecker.getRemComValue(1) != 9 && irsChecker.getRemComValue(1) != 0){
-				colourSensor.setColour(irsChecker.getRemComValue(1));
+				colourSensor.setColour(irsChecker.getRemComValue(1),m1);
 			}
 			//VÄRIN TUNNISTUS
-			colourSensor.findColour();
+			colourSensor.findColour(m3,m1);
 			//EXIT
 			if(Button.readButtons()==2) {
 				mA.rotateClaw(100);
