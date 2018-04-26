2ff69ed30b7fac46d6f9e9d4a90d174846398dcb
wisardsteri <visaso@metropolia.fi>
2018-04-26 12:21:16 +0300

    MusicPlayer

--- RoboProject/src/MusicPlayer.java
+++ RoboProject/src/MusicPlayer.java
@@ -1,15 +1,98 @@
 import lejos.hardware.Sound;
 
 public class MusicPlayer extends Thread {
+	   
+	   private int c;
+	   private int d;
+	   private int e;
+	   private int f;
+	   private int g;
+	   private int a;
+	   private int b;
+	   
+	   public MusicPlayer() {
+		   this.c = 261;
+		   this.d = 294;
+		   this.e = 330;
+		   this.f = 349;
+		   this.g = 392;
+		   this.a = 440;
+		   this.b = 494;
+		   
+	   }
 	
-	public MusicPlayer(String song) {
-		
-
-	}
 	
 	public void run() {
 		
+		   Sound.playNote(Sound.PIANO, c, 200);   
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, e, 400);
+		   Sound.pause(50);
+		   Sound.playNote(Sound.PIANO, e, 400);
+		   Sound.pause(200);
+		   
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, e, 200);   
+		   Sound.playNote(Sound.PIANO, c, 400);
+		   
+		   
+		   
+		   
+		   
+		   Sound.playNote(Sound.PIANO, e, 150);
+		   Sound.playNote(Sound.PIANO, e, 150);
+		   Sound.playNote(Sound.PIANO, e, 150);
+		   Sound.playNote(Sound.PIANO, f, 150);
+		   Sound.playNote(Sound.PIANO, g, 400);
+		   Sound.playNote(Sound.PIANO, g, 200);
+		   Sound.pause(100);
+		   
+		   Sound.playNote(Sound.PIANO, f, 150);   
+		   Sound.playNote(Sound.PIANO, e, 150);
+		   Sound.playNote(Sound.PIANO, f, 150);
+		   Sound.playNote(Sound.PIANO, g, 150);
+		   Sound.playNote(Sound.PIANO, e, 400);  
+		   
+		   
+		   
+		   //Verse 2
+		   Sound.playNote(Sound.PIANO, c, 150);
+		   Sound.playNote(Sound.PIANO, c, 150);
+		   Sound.playNote(Sound.PIANO, c, 150);   
+		   Sound.playNote(Sound.PIANO, b, 150);
+		   Sound.playNote(Sound.PIANO, b, 150);
+		   Sound.pause(150);
+		   Sound.playNote(Sound.PIANO, a, 150);
+		   Sound.playNote(Sound.PIANO, a, 150);   
+		   Sound.playNote(Sound.PIANO, a, 150);
+		   Sound.playNote(Sound.PIANO, a, 150);
+		   Sound.playNote(Sound.PIANO, g, 150);
+		   
+		   Sound.playNote(Sound.PIANO, c, 200);   
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, e, 400);
+		   Sound.pause(50);
+		   Sound.playNote(Sound.PIANO, e, 400);
+		   Sound.pause(200);
+		   
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, c, 200);
+		   Sound.playNote(Sound.PIANO, d, 200);
+		   Sound.playNote(Sound.PIANO, e, 200);   
+		   Sound.playNote(Sound.PIANO, c, 400);
+		   
+		   
+		   return;
+
+		   }
+		
 		//Sound.playTone(aFrequency, aDuration, aVolume);
-	}
+	
 	
 }
