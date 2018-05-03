ab785da2f73205c6dcee967066ca0efd144983ab
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2018-05-03 12:17:05 +0300

    Muutoksia
    Drive
    Colour
    Main
    Claw luokka luotu

---
+++ RoboProject/RoboProject/src/IrsChecker.java
@@ -0,0 +1,44 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.sensor.EV3IRSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class IrsChecker extends Thread {
+	private EV3IRSensor infraredSensor;
+	public IrsChecker(EV3IRSensor sensor) {
+		this.infraredSensor = sensor;
+	}
+
+	// THIS IS RUN WHEN THE THREAD IS STARTED
+	public void run() {
+		while (!Button.ENTER.isDown()) {
+			int remoteCommand = infraredSensor.getRemoteCommand(0);
+			if (remoteCommand != 0) {
+				LCD.drawString("Painoit: " + remoteCommand, 0, 0);
+				Delay.msDelay(500);
+			}
+		}
+	}
+	// METHOD TO GET REMOTECONTROLLER BUTTON VALUE WHEN PRESSED
+	public int getRemComValue(int channel) {
+		int remoteCommand = infraredSensor.getRemoteCommand(channel);
+		return remoteCommand;
+	}
+	public void close () {
+		infraredSensor.close();
+	}
+	// METHOD TO GET DISTANCE FROM OBJECTS NEAR SENSOR
+	public float getDistance() {
+		SampleProvider distance= infraredSensor.getDistanceMode();
+		float[] distSample = new float[distance.sampleSize()];
+		distance.fetchSample(distSample, 0);
+		return distSample[0];
+	}
+	public float getDirection() {
+		SampleProvider dir=infraredSensor.getSeekMode();
+		float[] dirSample = new float[dir.sampleSize()];
+		dir.fetchSample(dirSample, 0);
+		return dirSample[0];
+	}
+}
